ff7ae74006c5e12517bd7bf56e37202b3d7449eb
liliumrubellum <liliumrubellum@users.noreply.github.com>
2018-09-28 11:02:43 +0900

    expressの設定をenvに移動

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
+const moment = require('moment');
 const http = require('http');
 const express = require('express');
 const app = express();
 const bots = require('./bot-manager.js');
 
 app.get('/', function(request, response) {
-  console.log(Date.now() + " Ping Received");
+  console.log(moment().format('YYYY/MM/DD HH:mm:ss') + ' Ping Received');
   response.sendStatus(200);
 });
 
@@ -12,6 +13,8 @@ var listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
 
+const url = new URL(process.env.URL);
+url.port = process.env.PORT;
 setInterval(() => {
-  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+  http.get(url);
 }, 280000);
